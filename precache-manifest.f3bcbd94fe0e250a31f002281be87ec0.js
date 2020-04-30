c76d6ca0f6af43f73f49b571ca79c517bdc25cee
Jorge Alberto Landaverde <jorgealbertolandaverde@gmail.com>
2020-04-30 01:52:31 -0700

    Updates

---
+++ precache-manifest.f3bcbd94fe0e250a31f002281be87ec0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "256c389ac647b4edb57e1929d47504d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16d08d81222cdad91100",
+    "url": "/static/css/2.87d4210f.chunk.css"
+  },
+  {
+    "revision": "16d08d81222cdad91100",
+    "url": "/static/js/2.e6016f38.chunk.js"
+  },
+  {
+    "revision": "3780c1cb5da1f1b68e5488f5a00d0317",
+    "url": "/static/js/2.e6016f38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59b0b8a0e8f683f0464f",
+    "url": "/static/js/main.f8939764.chunk.js"
+  },
+  {
+    "revision": "4e7be396c02193fbe90a",
+    "url": "/static/js/runtime-main.f2e9d414.js"
+  }
+]);
